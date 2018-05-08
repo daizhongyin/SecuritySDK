d44d4e211fa9d8c14a4f37b026ef659bc705c029
Lin <97615274@qq.com>
2018-05-08 22:53:38 +0800

    日志封装1.0版本

--- securitysdkcore/src/main/java/com/nstl/securitysdkcore/Logger/SecuritySdkError.java
+++ securitysdkcore/src/main/java/com/nstl/securitysdkcore/Logger/SecuritySdkError.java
@@ -1,13 +1,20 @@
 package com.nstl.securitysdkcore.Logger;
 
+import android.app.admin.*;
+
 /**
  * Created by Lin on 2018/5/7.
  */
 
-public enum SecuritySdkError {
+public enum SecuritySdkError{
 
     FILE_OPEN_ERROR(10000, "文件打开错误"),
-    NETWORK_ERROR(10001,"网络异常错误");
+    NETWORK_ERROR(10001,"网络异常错误"),
+
+    //签名算法相关
+    NO_SUCCH_ALGORITHM(10002, "没有此类算法"),
+    SIGNATURE_ERROR(10002, "计算签名错误"),
+    INVALID_KEY(10003, "无效的密钥");
 
 
     private String msg;
---
+++ securitysdkcore/src/main/java/com/nstl/securitysdkcore/Logger/SecuritySdkTrace.java
@@ -0,0 +1,8 @@
+package com.nstl.securitysdkcore.Logger;
+
+/**
+ * Created by Lin on 2018/5/8.
+ */
+
+public enum SecuritySdkTrace {
+}
